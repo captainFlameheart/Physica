2704d66ad74c8f3cb2abf3ad54db71afb36954e9
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-02-13 14:05:32 +0100

    Extract game logic legacy.

---
+++ include/legacy/game_logic/game.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "game_environment/environment.h"
+//#include "macros.h"
+
+namespace legacy::game_logic
+{
+	void initialize(game_environment::Environment& environment);
+
+	void on_framebuffer_size_changed(
+		game_environment::Environment& environment,
+		int width, int height
+	);
+
+	void on_key_event(
+		game_environment::Environment& environment,
+		int const key,
+		int const scancode,
+		int const action,
+		int const mods
+	);
+
+	void on_cursor_event(
+		game_environment::Environment& environment,
+		double const x_pos,
+		double const y_pos
+	);
+
+	void on_mouse_button_event(
+		game_environment::Environment& environment,
+		int const button,
+		int const action,
+		int const mods
+	);
+
+	void on_scroll_event(
+		game_environment::Environment& environment,
+		double const x_offset,
+		double const y_offset
+	);
+
+	void tick(game_environment::Environment& environment);
+
+	void render(game_environment::Environment& environment);
+
+	void free(game_environment::Environment& environment);
+}
