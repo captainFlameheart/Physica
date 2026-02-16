1dc610fe389e04e298ac0a48d3160a86113a09fb
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-02-16 10:53:13 +0100

    Make point mass process dispatch indirect.

--- src/game_logic/binding_util/bind_GPU_only_buffer.cpp
+++ src/game_logic/binding_util/bind_GPU_only_buffer.cpp
@@ -7,6 +7,7 @@ namespace game_logic::binding_util
 	void bind_GPU_only_buffer(game_environment::Environment& environment, GLuint index)
 	{
 		GLuint buffer{ environment.state.buffers.GPU_only.buffers[index] };
+		
 		GLuint buffers[::game_state::bindings::shader_storage::count];
 		for (GLuint i{ 0u }; i < ::game_state::bindings::shader_storage::count; ++i)
 		{
@@ -17,6 +18,8 @@ namespace game_logic::binding_util
 			GL_SHADER_STORAGE_BUFFER, ::game_state::bindings::shader_storage::fixed_data, ::game_state::bindings::shader_storage::count, buffers
 		);
 		glBindBufferBase(GL_UNIFORM_BUFFER, ::game_state::bindings::uniform::fixed_data, buffer);
+		
+		glBindBuffer(GL_DISPATCH_INDIRECT_BUFFER, buffer);
 		glBindBuffer(GL_DRAW_INDIRECT_BUFFER, buffer);
 	}
 }
--- src/game_logic/tick/tick.cpp
+++ src/game_logic/tick/tick.cpp
@@ -1,12 +1,19 @@
 #include "game_logic/tick/include.h"
 #include "game_environment/environment.h"
+#include "game_state/layouts/include.h"
 
 namespace game_logic::tick
 {
 	void tick(game_environment::Environment& environment)
 	{
 		glUseProgram(environment.state.shaders.state.tick.state.bodies.state.point_masses.state.process);
-		glDispatchCompute(1u, 1u, 1u);	// TODO: Indirect
+		constexpr GLuint process_point_masses_index{ 0u };
+		GLintptr process_point_masses_command_offset
+		{
+			environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.offset +
+			process_point_masses_index * environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.top_level_array_stride
+		};
+		glDispatchComputeIndirect(static_cast<GLintptr>(process_point_masses_command_offset));
 		glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 	}
 }
