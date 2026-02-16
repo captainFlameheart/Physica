d7429342ae8f93c35195b724c7665b6c7dac8d30
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-02-16 18:02:02 +0100

    Automate body tick.

--- src/game_logic/tick/tick.cpp
+++ src/game_logic/tick/tick.cpp
@@ -16,16 +16,21 @@ namespace game_logic::tick
 		glDispatchCompute(update_tick_count_work_group_count, 1u, 1u);
 		glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT | GL_COMMAND_BARRIER_BIT);
 
-		glUseProgram(environment.state.shaders[static_cast<GLuint>(::game_state::shader_indices::tick::process_entities::bodies::Indices::point_masses)]);
-		constexpr GLuint process_point_masses_index{ 0u };
-		GLintptr process_point_masses_command_offset
+		for (GLuint tick_bodies_shader_index{ ::game_state::shader_indices::tick::process_entities::bodies::base }; tick_bodies_shader_index < ::game_state::shader_indices::tick::process_entities::bodies::end; ++tick_bodies_shader_index)
 		{
-			environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.offset +
-			process_point_masses_index * environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.top_level_array_stride
-		};
-		glDispatchComputeIndirect(static_cast<GLintptr>(process_point_masses_command_offset));
-		
+			glUseProgram(environment.state.shaders[tick_bodies_shader_index]);
+			GLuint index_in_tick_entities_shader_array{ tick_bodies_shader_index - ::game_state::shader_indices::tick::process_entities::base };
+			GLintptr command_offset
+			{
+				environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.offset +
+				index_in_tick_entities_shader_array * environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.top_level_array_stride
+			};
+			glDispatchComputeIndirect(static_cast<GLintptr>(command_offset));
+		}
+
 		// IMPORTANT TODO: Only have GL_UNIFORM_BARRIER_BIT right before drawing
 		glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT | GL_UNIFORM_BARRIER_BIT);
+
+		// TODO: Constraints
 	}
 }
