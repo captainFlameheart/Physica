a1848dcc4d9a21371d2417bb6707b4a90ffc469a
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-02-18 11:56:32 +0100

    Create rope! :D

--- include/game_state/units/units.h
+++ include/game_state/units/units.h
@@ -3,10 +3,10 @@
 
 namespace game_state::units
 {
-	constexpr GLfloat meter_in_length_units{ 1000.0f };
+	constexpr GLfloat meter_in_length_units{ 1000000.0f };
 	constexpr GLfloat length_unit_in_meters{ 1.0f / meter_in_length_units };
 
-	constexpr GLfloat second_in_time_units{ 60.0f };
+	constexpr GLfloat second_in_time_units{ 8.0f * 60.0f };
 	constexpr GLfloat time_unit_in_seconds{ 1.0f / second_in_time_units };
 
 	constexpr GLfloat meters_per_second_in_length_units_per_time_unit{ meter_in_length_units / second_in_time_units };
--- src/game_logic/tick/tick.cpp
+++ src/game_logic/tick/tick.cpp
@@ -33,10 +33,28 @@ namespace game_logic::tick
 				glDispatchComputeIndirect(static_cast<GLintptr>(command_offset));
 			}
 
+			glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+
+			for (GLuint tick_constraints_shader_index{ ::game_state::shader_indices::tick::process_entities::constraints::base }; tick_constraints_shader_index < ::game_state::shader_indices::tick::process_entities::constraints::end; ++tick_constraints_shader_index)
+			{
+				/*if (tick_constraints_shader_index != ::game_state::shader_indices::tick::process_entities::constraints::Indices::point_mass_distance_constraints)
+				{
+					return;
+				}*/
+
+				glUseProgram(environment.state.shaders[tick_constraints_shader_index]);
+				GLuint index_in_tick_entities_shader_array{ tick_constraints_shader_index - ::game_state::shader_indices::tick::process_entities::base };
+				GLintptr command_offset
+				{
+					environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.offset +
+					index_in_tick_entities_shader_array * environment.state.layouts.fixed_data.dispatch_commands_work_group_count_x_state.top_level_array_stride
+				};
+				glDispatchComputeIndirect(static_cast<GLintptr>(command_offset));
+			}
+
 			// IMPORTANT TODO: Only have GL_UNIFORM_BARRIER_BIT right before drawing
 			glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT | GL_UNIFORM_BARRIER_BIT);
 
-			// TODO: Constraints
 		}
 
 		++environment.state.tick_count;
