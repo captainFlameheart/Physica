0ff13ea389cb7b0f8cc593290472784f1075fe48
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-03-11 19:29:28 +0100

    Compile plan compaction shader.

--- src/game_logic/initialize/compile_shaders/compile_shaders.cpp
+++ src/game_logic/initialize/compile_shaders/compile_shaders.cpp
@@ -24,6 +24,8 @@ namespace game_logic::initialize::compile_shaders
 
 		compile_commit_constraint_spawner_counts(environment, compile_environment);
 
+		compile_plan_rigid_body_circle_contact_constraint_spawner_compaction(environment, compile_environment);
+
 		compile_tick_rigid_body_circle_contact_constraint_spawners(environment, compile_environment);
 
 		compile_tick_point_mass_distance_constraints(environment, compile_environment);
