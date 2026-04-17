ebb83478fac4cb323c8b88765394d2f98537059c
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-04-17 17:55:48 +0200

    Set up Holographic Radiance Cascades source framebuffer.

--- include/game_logic/initialize/holographic_radiance_cascades/include.h
+++ include/game_logic/initialize/holographic_radiance_cascades/include.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "initialize.h"
+#include <iostream>
--- src/game_logic/initialize/holographic_radiance_cascade/initialize.cpp
+++ src/game_logic/initialize/holographic_radiance_cascade/initialize.cpp
@@ -1,6 +1,7 @@
 #include "glad_glfw.h"
 #include "game_environment/Environment.h"
 #include "game_logic/initialize/holographic_radiance_cascades/include.h"
+#include <array>
 
 namespace game_logic::initialize::holographic_radiance_cascades
 {
@@ -13,5 +14,26 @@ namespace game_logic::initialize::holographic_radiance_cascades
 			environment.state.holographic_radiance_cascades.source_texture, ::game_state::holographic_radiance_cascades::source_layers::count,
 			GL_RGBA32F, environment.state.glfw.framebuffer_width, environment.state.glfw.framebuffer_height
 		);
+
+		glCreateFramebuffers(1u, &environment.state.holographic_radiance_cascades.source_framebuffer);
+		GLenum source_draw_buffers[::game_state::holographic_radiance_cascades::source_layers::count];
+		for (GLuint layer{ 0u }; layer < ::game_state::holographic_radiance_cascades::source_layers::count; ++layer)
+		{
+			GLenum color_attachment{ GL_COLOR_ATTACHMENT0 + layer };
+			glNamedFramebufferTextureLayer
+			(
+				environment.state.holographic_radiance_cascades.source_framebuffer,
+				color_attachment,
+				environment.state.holographic_radiance_cascades.source_texture, 0, layer
+			);
+			source_draw_buffers[layer] = color_attachment;
+		}
+		glNamedFramebufferDrawBuffers(environment.state.holographic_radiance_cascades.source_framebuffer, std::size(source_draw_buffers), source_draw_buffers);
+
+		GLenum source_framebuffer_status{ glCheckFramebufferStatus(GL_FRAMEBUFFER) };
+		if (source_framebuffer_status != GL_FRAMEBUFFER_COMPLETE)
+		{
+			std::cerr << "Holographic Radiance Cascades source framebuffer invalid, status: " << source_framebuffer_status << std::endl;
+		}
 	}
 }
