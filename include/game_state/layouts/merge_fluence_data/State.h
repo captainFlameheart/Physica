c0ab33af1149336ec3ca80392399657f7b209fae
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-04-20 14:40:44 +0200

    Query merge fluence data layout.

--- include/game_state/layouts/merge_fluence_data/State.h
+++ include/game_state/layouts/merge_fluence_data/State.h
@@ -2,6 +2,66 @@
 
 namespace game_state::layouts::merge_fluence_data
 {
+	struct direction_mask
+	{
+		GLint offset;
+	};
+
+	struct cascade
+	{
+		GLint offset;
+	};
+
+	struct max_ray_probe_column
+	{
+		GLint offset;
+	};
+
+	struct max_fluence_probe_column_texel_x
+	{
+		GLint offset;
+	};
+
+	struct max_fluence_probe_y
+	{
+		GLint offset;
+	};
+
+	struct rays_per_probe
+	{
+		GLint offset;
+	};
+
+	struct cascade_power_of_two
+	{
+		GLint offset;
+	};
+
+	struct upper_cascade_probe_column_texel_x_mask
+	{
+		GLint offset;
+	};
+
+	struct upper_cascade
+	{
+		GLint offset;
+	};
+
+	struct upper_cascade_fluence_layer
+	{
+		GLint offset;
+	};
+
+	struct output_factor
+	{
+		GLint offset;
+	};
+
+	struct output_shift
+	{
+		GLint offset;
+	};
+
 	struct Block
 	{
 		GLint buffer_data_size;
@@ -9,6 +69,66 @@ namespace game_state::layouts::merge_fluence_data
 
 	struct State
 	{
+		union
+		{
+			direction_mask direction_mask_state;
+			GLint direction_mask_properties[sizeof(direction_mask) / sizeof(GLint)];
+		};
+		union
+		{
+			cascade cascade_state;
+			GLint cascade_properties[sizeof(cascade) / sizeof(GLint)];
+		};
+		union
+		{
+			max_ray_probe_column max_ray_probe_column_state;
+			GLint max_ray_probe_column_properties[sizeof(max_ray_probe_column) / sizeof(GLint)];
+		};
+		union
+		{
+			max_fluence_probe_column_texel_x max_fluence_probe_column_texel_x_state;
+			GLint max_fluence_probe_column_texel_x_properties[sizeof(max_fluence_probe_column_texel_x) / sizeof(GLint)];
+		};
+		union
+		{
+			max_fluence_probe_y max_fluence_probe_y_state;
+			GLint max_fluence_probe_y_properties[sizeof(max_fluence_probe_y) / sizeof(GLint)];
+		};
+		union
+		{
+			rays_per_probe rays_per_probe_state;
+			GLint rays_per_probe_properties[sizeof(rays_per_probe) / sizeof(GLint)];
+		};
+		union
+		{
+			cascade_power_of_two cascade_power_of_two_state;
+			GLint cascade_power_of_two_properties[sizeof(cascade_power_of_two) / sizeof(GLint)];
+		};
+		union
+		{
+			upper_cascade_probe_column_texel_x_mask upper_cascade_probe_column_texel_x_mask_state;
+			GLint upper_cascade_probe_column_texel_x_mask_properties[sizeof(upper_cascade_probe_column_texel_x_mask) / sizeof(GLint)];
+		};
+		union
+		{
+			upper_cascade upper_cascade_state;
+			GLint upper_cascade_properties[sizeof(upper_cascade) / sizeof(GLint)];
+		};
+		union
+		{
+			upper_cascade_fluence_layer upper_cascade_fluence_layer_state;
+			GLint upper_cascade_fluence_layer_properties[sizeof(upper_cascade_fluence_layer) / sizeof(GLint)];
+		};
+		union
+		{
+			output_factor output_factor_state;
+			GLint output_factor_properties[sizeof(output_factor) / sizeof(GLint)];
+		};
+		union
+		{
+			output_shift output_shift_state;
+			GLint output_shift_properties[sizeof(output_shift) / sizeof(GLint)];
+		};
 		union
 		{
 			Block block_state;
--- src/game_logic/initialize/query_layouts/query_merge_fluence_data_layout.cpp
+++ src/game_logic/initialize/query_layouts/query_merge_fluence_data_layout.cpp
@@ -7,6 +7,66 @@ namespace game_logic::initialize::query_layouts
 {
 	void query_merge_fluence_data_layout(game_environment::Environment& environment, GLuint program)
 	{
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.direction_mask",
+			environment.state.layouts.merge_fluence_data.direction_mask_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.cascade",
+			environment.state.layouts.merge_fluence_data.cascade_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.max_ray_probe_column",
+			environment.state.layouts.merge_fluence_data.max_ray_probe_column_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.max_fluence_probe_column_texel_x",
+			environment.state.layouts.merge_fluence_data.max_fluence_probe_column_texel_x_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.max_fluence_probe_y",
+			environment.state.layouts.merge_fluence_data.max_fluence_probe_y_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.rays_per_probe",
+			environment.state.layouts.merge_fluence_data.rays_per_probe_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.cascade_power_of_two",
+			environment.state.layouts.merge_fluence_data.cascade_power_of_two_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.upper_cascade_probe_column_texel_x_mask",
+			environment.state.layouts.merge_fluence_data.upper_cascade_probe_column_texel_x_mask_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.upper_cascade",
+			environment.state.layouts.merge_fluence_data.upper_cascade_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.upper_cascade_fluence_layer",
+			environment.state.layouts.merge_fluence_data.upper_cascade_fluence_layer_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.output_factor",
+			environment.state.layouts.merge_fluence_data.output_factor_properties
+		);
+		::game_logic::shader_util::query::query_uniform_variable_offset
+		(
+			program, "Merge_Fluence_Data.output_shift",
+			environment.state.layouts.merge_fluence_data.output_shift_properties
+		);
 		::game_logic::shader_util::query::query_uniform_buffer_data_size
 		(
 			program, "Merge_Fluence_Data",
