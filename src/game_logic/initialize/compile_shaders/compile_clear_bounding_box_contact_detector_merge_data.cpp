19c6d69192ba8e60be3d82e4f4377862533c178e
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-04-30 17:41:36 +0200

    Compile clear detector merge data shader.

---
+++ src/game_logic/initialize/compile_shaders/compile_clear_bounding_box_contact_detector_merge_data.cpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "glad_glfw.h"
+#include "game_environment/environment.h"
+#include "game_state/initialize/compile_shaders/environment/include.h"
+#include "util/shader/shader.h"
+#include "game_logic/shader_util/include.h"
+
+namespace game_logic::initialize::compile_shaders
+{
+	void compile_clear_bounding_box_contact_detector_merge_data
+	(
+		game_environment::Environment& environment, ::game_state::initialize::compile_shaders::environment::Environment compile_environment
+	)
+	{
+		::util::shader::set_shader_statically
+		(
+			compile_environment.shader_group.compute_shader,
+			compile_environment.writable_prefix_source,
+			::util::shader::file_to_string("tick/constraint_spawners/clear_bounding_box_contact_detector_merge_data.comp")
+		);
+
+		environment.state.shaders[static_cast<GLuint>(::game_state::shader_indices::tick::process_entities::constraint_spawners::Indices::clear_bounding_box_contact_detector_merge_data)] = ::util::shader::create_program
+		(
+			compile_environment.shader_group.compute_shader
+		);
+	}
+}
