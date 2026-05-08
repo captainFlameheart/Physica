0d790a3dce55d6d0fad576e0724d0cb9bf3f40b2
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2026-05-08 13:41:47 +0200

    Compile clear invalid circle contacts shader.

--- src/game_logic/initialize/compile_shaders/compile_shaders.cpp
+++ src/game_logic/initialize/compile_shaders/compile_shaders.cpp
@@ -14,11 +14,13 @@ namespace game_logic::initialize::compile_shaders
 
 		compile_initialize_bounding_box_contact_detector_metadata(environment, compile_environment);
 		compile_initialize_bounding_box_contact_detectors(environment, compile_environment);
-		compile_set_clear_invalid_contact_commands(environment, compile_environment);
 		compile_reset_bounding_box_contact_detector_metadata(environment, compile_environment);
 		compile_reset_bounding_box_contact_detectors(environment, compile_environment);
 		compile_reset_bounding_box_contact_detector_deaths(environment, compile_environment);
+		compile_set_clear_invalid_contact_commands(environment, compile_environment);
+
 		compile_clear_invalid_rigid_body_circle_contact_detectors(environment, compile_environment);
+		compile_clear_invalid_rigid_body_circle_contacts(environment, compile_environment);
 
 		compile_initialize(environment, compile_environment);
 		
